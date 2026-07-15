a05a994fbade3bc3ca103a4f3be14688533e0717
Dan Walsh <dan@danwalsh.ca>
2026-07-15 19:25:17 -0400

    Updated config params

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,8 +4,8 @@ export const SITE = {
   email: 'anas.oliveoil@gmail.com',
   themeColor: '#001263',
   locale: 'en_CA',
-  importer: 'Karteros Enterprises',
-  location: 'Brampton, Ontario, Canada',
+  companyName: 'Karteros Enterprises',
+  companyAddress: 'Brampton, Ontario, Canada',
 } as const;
 
 export const FORMSUBMIT_ACTION = `https://formsubmit.co/${SITE.email}`;
